f0ef9c2dd0a1a5ec64b1440672d4b80531f1df07
mmarshall <mmarshall@ivenix.com>
2017-03-07 09:43:57 -0500

    Initial commit.

---
+++ com.ivenix.debug.gdbjtag/src/com/ivenix/debug/gdbjtag/preferences/ColorReadOnlyFactory.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Liviu Ionescu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Liviu Ionescu - initial version
+ *******************************************************************************/
+
+package com.ivenix.debug.gdbjtag.preferences;
+
+import com.ivenix.debug.gdbjtag.PeripheralsColorFactory;
+import com.ivenix.debug.gdbjtag.PersistentPreferences;
+import com.ivenix.debug.gdbjtag.render.peripheral.PeripheralColumnLabelProvider;
+
+public class ColorReadOnlyFactory extends PeripheralsColorFactory {
+
+	public ColorReadOnlyFactory() {
+		super(PeripheralColumnLabelProvider.COLOR_READONLY, PersistentPreferences.PERIPHERALS_COLOR_READONLY);
+	}
+}
